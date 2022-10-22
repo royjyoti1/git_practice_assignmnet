e1fd5ae4599fc07afc917a893250ed0b17bab9b1
royjyoti1 <royjyoti424@gmail.com>
2022-10-22 22:34:49 +0530

    check this program

---
+++ pallindrome.js
@@ -0,0 +1,9 @@
+let rev_num=0;
+for(let i =num;i>=1:Math.floor(1/10)){
+    rev_num=rev_num*10+i%10
+}
+if(rev_num==num){
+    console.log("palindrome num");
+}else
+    console.log("No [allindrome num");
+}
---
+++ prime.js
@@ -0,0 +1,12 @@
+let num=4;
+let count=0;
+for(let i=1;i<=num;i++){
+    if(num%1=0)
+        count++
+
+    }
+} if (count==2){
+    console.log("Num is prime");
+}else{
+    console.log("Num is not prime");
+}
